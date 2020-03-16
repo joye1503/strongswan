b78042dd87769746abad9f463dbf9246df0e9c92
Noel Kuntze <noel@familie-kuntze.de>
2020-03-16 08:36:57 +0100

    Exit script if curl or unzip fail

--- scripts/test.sh
+++ scripts/test.sh
@@ -443,8 +443,8 @@ sonarcloud)
             export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
             curl --create-dirs -sSLo $HOME/.sonar/sonar-scanner.zip \
                 https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/\
-sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip 
-            unzip -o $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/
+sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip || exit $?
+            unzip -o $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/ || exit $?
             export PATH=$SONAR_SCANNER_HOME/bin:$PATH
             export SONAR_SCANNER_OPTS="-server"
 
