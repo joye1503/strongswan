d28db143c4e33d3d1af3f68a9395447aa2841711
Noel Kuntze <noel@familie-kuntze.de>
2020-03-16 08:36:30 +0100

    Fix sonar-scanner URL

--- scripts/test.sh
+++ scripts/test.sh
@@ -442,8 +442,8 @@ sonarcloud)
             export SONAR_SCANNER_VERSION=4.2.0.1873
             export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
             curl --create-dirs -sSLo $HOME/.sonar/sonar-scanner.zip \
-                https://binaries.sonarsource.com/Distribution/sonar-scanner-cli\
-                /sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip 
+                https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/\
+sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip 
             unzip -o $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/
             export PATH=$SONAR_SCANNER_HOME/bin:$PATH
             export SONAR_SCANNER_OPTS="-server"
