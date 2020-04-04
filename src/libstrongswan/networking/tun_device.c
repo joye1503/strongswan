c1fc31917a16c4c458bc8168da5c6826dbceeda3
Noel Kuntze <noel@familie-kuntze.de>
2020-04-04 20:28:17 +0200

    Helper functions windows_drv_info_get_next_hardwareid and find_matching_hardwareid, add newline after function header of write_to_ring

--- src/libstrongswan/networking/tun_device.c
+++ src/libstrongswan/networking/tun_device.c
@@ -51,6 +51,8 @@ tun_device_t *tun_device_create(const char *name_tmpl)
 #include <cfgmgr32.h>
 #include <ntstrsafe.h>
 #include <ddk/ndisguid.h>
+#include <setupapi.h>
+#include <windows.h>
 #define IFNAMSIZ 256
 #else
 #include <sys/ioctl.h>
@@ -145,8 +147,70 @@ static inline bool ring_over_capacity(TUN_RING *ring)
     return ((ring->Head >= TUN_RING_CAPACITY) || (ring->Tail >= TUN_RING_CAPACITY));
 }
 
+/*
+ * Helper function to get the next HardwareID in the
+ * SP_DRVINFO_DETAIL_DATA_A.HardwareID linear array.
+ * (Looks like this: "abcdef\0ghijkl\0mnopqrs\0\0". Empty string (Just a NULL byte)
+ * denotes the end of the array)
+ *
+ * @param pile			linear character array of the hardwareIDs as they are part of
+ *						the SP_DRVINFO_DETAIL_DATA_A struct.
+ * @param offset		pointer to a location in which the end position of the search algorithm is stored.
+ *						It is used to keep the offset between multiple calls of the function and ease its use.
+ *						The value is the position at which the resulting string ends (strlen() + 1, or its NULL byte)
+ * @return				The next hardwareID or NULL, if the end of the array was reached.
+ */
+char *windows_drv_info_get_next_hardwareid(char *pile, size_t *offset)
+{
+	size_t len = 0, old_offset = 0;
+	while(true)
+	{
+		old_offset = *offset;
+		len = strlen(pile + *offset);
+		*offset += len + 1;
+		
+		if (len == 0)
+		{
+			/* End of the list, empty string. */
+			return NULL;
+		}
+
+		return pile + old_offset;
+	}
+}
+
+/*
+ * Helper function that wraps around windows_drv_info_get_next_hardwareid.
+ * It returns true if needle is in the pile (pile is a linear, empty string terminated array of strings).
+ * It returns false if needle is NOT in the pile.
+ *
+ * @param pile			A linear array of strings. Terminated by an empty string.
+ * @param needle		A string to find in pile.
+ *
+ * @returns				Returns whether needle is in pile
+ */
+bool find_matching_hardwareid(char *pile, char* needle)
+{
+	size_t offset = 0;
+	char *item;
+	while(true)
+	{
+		item = windows_drv_info_get_next_hardwareid(pile, &offset);
+		if (!item)
+		{
+			return false;
+		}
+
+		if(strcmp(item, needle) == 0)
+		{
+			return true;
+		}
+	}
+}
+
 /* This is likely broken (!!!) */
-static bool write_to_ring(TUN_RING *ring, chunk_t packet) {
+static bool write_to_ring(TUN_RING *ring, chunk_t packet)
+{
         /* Check if packet fits */
     TUN_PACKET *tun_packet;
     /* TODO: if ring is full or over capacity, wait until wintun driver sends event */
