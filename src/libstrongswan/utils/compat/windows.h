5460bbe49b81024cbe601cfce67986fdff35e249
Noel Kuntze <noel@familie-kuntze.de>
2020-04-09 19:05:50 +0200

    Expand description of registry_wait_get_value for contents of caller_buf and caller_buf_len when function returns FALSE

--- src/libstrongswan/utils/compat/windows.h
+++ src/libstrongswan/utils/compat/windows.h
@@ -284,8 +284,10 @@ void dlerror_mt(char *buf, size_t buf_len);
  * helper function for getting registry values that may not exist yet
  * @param	key				registry key to query
  * @param	caller_buf 		caller supplied buffer for storing the queried value. Must be realloc()-atable
+ *							If the function returns FALSE, the content of caller_buf is undefined.
  * @param	caller_buf_len	length of caller_buf in bytes.
  							Will contain the length of the written data in bytes after the function returns.
+ 							If the function returns FALSE, the content of caller_buf_len is undefined.
  * @param	reg_val_name	name of the value to query
  * @param	reg_val_type	Will contain the registry type of the value (MULTI_SZ, EXPAND_SZ, ...)
  * @param	timeout			time (in ms) to wait for the registry key to appear
