853697ae20a03e485adc3bb9fe332ec6a1060521
Noel Kuntze <noel@familie-kuntze.de>
2020-05-17 06:43:22 +0200

    Move wintun structs and defines into wintun_support.h

--- src/libstrongswan/networking/wintun_support.h
+++ src/libstrongswan/networking/wintun_support.h
@@ -32,6 +32,47 @@ extern "C" {
 #include "windows_tun.h"
 #include "tun_device.h"
 
+/* capacity must be a power of two and between 128 kiB and 64 MiB */
+#define IFNAMSIZ 256
+#define TUN_PACKET_ALIGNMENT 4
+#define TUN_RING_CAPACITY (64*1024*1024)
+#define TUN_RING_SIZE(TUN_RING, capacity) (sizeof(TUN_RING) + capacity + 0x10000)
+#define TUN_MAX_IP_PACKET_SIZE 0xFFFF
+#define PNP_INSTANCE_ID "{abcde}"
+#define WINTUN_COMPONENT_ID "Wintun"
+/* for use with SetDeviceRegistryPropertyString */
+#define STRONGSWAN_WINTUN_INTERFACE_NAME "strongSwan VPN adapter"
+/* Randomly generated, globally unique GUID for the strongSwan VPN adapter*/
+const static GUID GUID_WINTUN_STRONGSWAN = { 0x1f1f4dd1L, 0xe4d8, 0x487d, { 0xb7, 0xd5, 0x24, 0x93, 0xb2, 0xce, 0x0c, 0x49 } };
+/* GUIDs from openvpn tun.c code */
+const static GUID GUID_DEVCLASS_NET = { 0x4d36e972L, 0xe325, 0x11ce, { 0xbf, 0xc1, 0x08, 0x00, 0x2b, 0xe1, 0x03, 0x18 } };
+#define TUN_IOCTL_REGISTER_RINGS 0xca6ce5c0
+#define TUN_PACKET_TRAILING_SIZE (sizeof(uint32_t) + ((TUN_MAX_IP_PACKET_SIZE + \
+    (TUN_PACKET_ALIGNMENT - 1)) &~(TUN_PACKET_ALIGNMENT - 1)) - TUN_PACKET_ALIGNMENT)
+#define TUN_WRAP_POSITION(position, size) ({position & (size - 1);})
+#define TUN_PACKET_ALIGN(size) ((size + (TUN_PACKET_ALIGNMENT - 1)) &~(TUN_PACKET_ALIGNMENT - 1))
+typedef struct _TUN_RING {
+    volatile ULONG Head;
+    volatile ULONG Tail;
+    volatile LONG Alertable;
+    UCHAR Data[TUN_RING_CAPACITY + TUN_PACKET_TRAILING_SIZE];
+} TUN_RING;
+
+typedef struct _TUN_REGISTER_RINGS {
+    struct {
+        ULONG RingSize;
+        TUN_RING *Ring;
+        HANDLE TailMoved;
+    } Send, Receive;
+    /* Send ring is for data from driver to application */
+    /* Receive ring is for data from application to driver */
+} TUN_REGISTER_RINGS;
+
+typedef struct _TUN_PACKET {
+    ULONG Size;
+    UCHAR Data[TUN_MAX_IP_PACKET_SIZE];
+} TUN_PACKET;
+
 tun_device_t *try_configure_wintun(const char *name_tmpl);
 
 #        ifdef __cplusplus
